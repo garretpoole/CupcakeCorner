733c34c791f3ea4dce4fb34cfe07cd2360efcdd1
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-28 15:53:29 -0700

    loading AsyncImage from URL remote server

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,51 +7,20 @@
 
 import SwiftUI
 
-struct Response: Codable{
-    var results: [Result]
-}
-
-struct Result: Codable{
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
     
     var body: some View {
-        List(results, id: \.trackId){ item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
+        //SwiftUI doesnt know the size of the image coming from the internet
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
         }
-        .task{
-            //'await' must be used for async functions
-            await loadData()
-        }
-    }
-    //downloads codable data
-    func loadData() async{
-        //structure for apple api
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")
-        else{
-            print("Invalid URL")
-            return
-        }
-        
-        do{
-            //return is a tuple with data and metadata
-            let (data, _) = try await URLSession.shared.data(from: url)
-            //convert data object into Response
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
-                results = decodedResponse.results
-            }
-        } catch{
-            print("Invalid data")
-        }
+        //resizable() and .frame() cannot apply here to AsyncImage bc Image has yet to download
+        //applies to placeholder view
+        .frame(width: 200, height: 200)
     }
 }
 
