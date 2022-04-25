1ba478953435c0ad3de396d8501d985c416e5ca0
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-24 22:18:25 -0700

    hide checkout view images voice over

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -21,8 +21,10 @@ struct CheckoutView: View {
                     image
                         .resizable()
                         .scaledToFit()
+                        .accessibilityHidden(true)
                 } placeholder: {
                     ProgressView()
+                        .accessibilityHidden(true)
                 }
                 .frame(height: 233)
                 
